45dcc65422f0ba04835093831a60ef3a26216128
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-01 21:54:55 +0100

    Connected to Mongodb

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 //we import the express framework 
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
 
+//DB config. We use the mongo key stored with the vaiable MongoURI
+//in the keys.js file under the config folder
+const db = require('./config/keys').MongoURI;
+
+//connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true})
+   .then(() => console.log('MongoDB connected...'))
+   .catch(err => console.log(err));
+console.log("brick");
 //EJS
+//to handle layouts
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
