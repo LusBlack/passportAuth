6b648b6aa1c642e70b7beccbd34a6cf18f6327df
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-03 22:19:58 +0100

    Had to reconfigure string key again

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://louikwuegbu:BeachMaster02@cluster0.7wpne3t.mongodb.net/?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://louikwuegbu:BeachMaster02@cluster0.aongv0p.mongodb.net/?retryWrites=true&w=majority'
 }
 
 //test
