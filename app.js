386736c36b0ed4c666e3a009c77df330aa6aab8a
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-12 18:54:09 +0100

    express session and added passport config

--- app.js
+++ app.js
@@ -2,6 +2,14 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+//const passport = require('./config/passport');
+const passport = require('passport')
+
+
+//passport config 
+require('./config/passport')(passport)
 
 const app = express();
 
@@ -24,6 +32,27 @@ app.set('view engine', 'ejs');
 //Bodyparser 
 //to handle and process data sent from client-side forms 
 app.use(express.urlencoded({ extended: false }));
+
+//express session
+app.use(session({
+   secret: 'secret',
+   resave: true,
+   saveUninitialized: true
+}));
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connect flash
+app.use(flash()); 
+
+//global vars
+app.use((req, res, next) => {
+   res.locals.success_msg = req.flash('success_msg');
+   res.locals.error_msg = req.flash('error_msg');
+   next();
+});
 
 //Routes
 app.use('/', require('./routes/index'));
