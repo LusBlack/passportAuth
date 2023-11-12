6576aca19fa6e6e1ac83411656ee3b623b5d0849
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-13 00:25:18 +0100

    added serializers and deserializers

--- config/passport.js
+++ config/passport.js
@@ -1,21 +1,23 @@
 const LocalStrategy = require('passport-local').Strategy;
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 //load user model
 const User = require('../models/User');
 
+ 
 module.exports= function(passport) {
     passport.use(
         new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
             //Match user
-            User.findOne({ email: email })
-            .then(user => {
+            User.findOne({ 
+                email: email 
+            }).then(user => {
                 if(!user) {
                     return done(null, false, { message: 'That email is not registered' });
                 }
-            })
-            .catch(err => console.log(err));
+            
+           // .catch(err => console.log(err));
 
             //match password
             bcrypt.compare(password, user.password, (err, isMatch) => {
@@ -26,7 +28,35 @@ module.exports= function(passport) {
                 } else {
                     return done(null, false, { message: 'Password incorrect' })
                 }
-            })
-        })
-    );
-}
+            });
+        });
+    }),
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+      }))
+    
+    
+      passport.deserializeUser((id, done) => {
+        const user = users.find(u => u.id === id);
+        done(null, user);
+      });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
