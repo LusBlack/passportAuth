ee6563171051018fc3123476135f1a99b215e0d5
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-13 00:25:36 +0100

    dashboaard route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+//welcome page
 router.get('/', (req, res) => res.render('welcome'));
+
+//dashboard
+router.get('/dashboard', (req, res) => res.render('dashboard'));
 
 module.exports = router;
 // test
